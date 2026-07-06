7214fa15c333b41a05547b24e354d8966d54e993
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:10:57 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "api_management_product_apis" {
   description = "All api_management_product_api resources"
   value       = azurerm_api_management_product_api.api_management_product_apis
 }
+output "api_management_product_apis_api_management_name" {
+  description = "List of api_management_name values across all api_management_product_apis"
+  value       = [for k, v in azurerm_api_management_product_api.api_management_product_apis : v.api_management_name]
+}
+output "api_management_product_apis_api_name" {
+  description = "List of api_name values across all api_management_product_apis"
+  value       = [for k, v in azurerm_api_management_product_api.api_management_product_apis : v.api_name]
+}
+output "api_management_product_apis_product_id" {
+  description = "List of product_id values across all api_management_product_apis"
+  value       = [for k, v in azurerm_api_management_product_api.api_management_product_apis : v.product_id]
+}
+output "api_management_product_apis_resource_group_name" {
+  description = "List of resource_group_name values across all api_management_product_apis"
+  value       = [for k, v in azurerm_api_management_product_api.api_management_product_apis : v.resource_group_name]
+}
 
